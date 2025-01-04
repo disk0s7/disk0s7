6d9ab553f09eefe18217782e4ec41bcb7620b38d
disk0s7 <disk0s7@proton.me>
2025-01-04 15:39:12 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://disk0s7.github.io',
+    site: 'https://disk0.io',
     integrations: [
         mdx(),
         sitemap(),
